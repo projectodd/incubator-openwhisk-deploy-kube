f531c969f41db0a05b3d92ed56f29e628e83678b
Ruben Romero Montes <rromerom@redhat.com>
2018-02-06 17:48:35 +0100

    Only initialize the first time

--- docker/couchdb/init.sh
+++ docker/couchdb/init.sh
@@ -1,84 +1,94 @@
 #!/bin/bash
 set -ex
 
-pushd /openwhisk
+# start couchdb as a background process
+/docker-entrypoint.sh /opt/couchdb/bin/couchdb &
 
-  # if auth guest overwrite file
-  if [ -n "$AUTH_GUEST" ]; then
-    echo "$AUTH_GUEST" > /openwhisk/ansible/files/auth.guest
-  fi
-
-  # if auth whisk system overwrite file
-  if [ -n "$AUTH_WHISK_SYSTEM" ]; then
-    echo "$AUTH_WHISK_SYSTEM" > /openwhisk/ansible/files/auth.whisk.system
-  fi
-
-  # start couchdb with a background process
-  /docker-entrypoint.sh /opt/couchdb/bin/couchdb &
-
-  # wait for couchdb to be up and running
-  TIMEOUT=0
-  echo "wait for CouchDB to be up and running"
-  until $( curl --output /dev/null --silent http://$DB_HOST:$DB_PORT/_utils ) || [ $TIMEOUT -eq 30 ]; do
+# wait for couchdb to be up and running
+TIMEOUT=0
+echo "wait for CouchDB to be up and running"
+until $( curl --output /dev/null --silent http://$DB_HOST:$DB_PORT/_utils ) || [ $TIMEOUT -eq 30 ]; do
     echo "waiting for CouchDB to be available"
 
     sleep 2
     let TIMEOUT=TIMEOUT+1
-  done
+done
 
-  if [ $TIMEOUT -eq 30 ]; then
+if [ $TIMEOUT -eq 30 ]; then
     echo "failed to setup CouchDB"
     exit 1
-  fi
+fi
 
-  # Fake our UID because OpenShift runs with random uids
-  export LD_PRELOAD=/usr/lib/libuid_wrapper.so
-  export UID_WRAPPER=1
-  export UID_WRAPPER_ROOT=1
 
-  # setup and initialize DB
-  pushd ansible
-    ansible-playbook -i environments/local setup.yml
-    ansible-playbook -i environments/local couchdb.yml --tags ini \
-      -e db_prefix=$DB_PREFIX \
-      -e db_host=localhost \
-      -e db_username=$COUCHDB_USER \
-      -e db_password=$COUCHDB_PASSWORD \
-      -e db_port=$DB_PORT \
-      -e openwhisk_home=/openwhisk
-  popd
+if [[ -f /opt/couchdb/data/_openwhisk_initialized.stamp ]]; then
+    # If we mounted an existing database from the persistent volume then use it.
+    echo "_openwhisk_initialized.stamp exists; using existing database"
+else
+    # No existing database; we will have to initialize it.
+    pushd /openwhisk
+        # if auth guest overwrite file
+        if [ -n "$AUTH_GUEST" ]; then
+            echo "$AUTH_GUEST" > /openwhisk/ansible/files/auth.guest
+        fi
 
-  # disable reduce limits on views
-  curl -X PUT http://$COUCHDB_USER:$COUCHDB_PASSWORD@localhost:$DB_PORT/_node/couchdb@$NODENAME/_config/query_server_config/reduce_limit -d '"false"'
+        # if auth whisk system overwrite file
+        if [ -n "$AUTH_WHISK_SYSTEM" ]; then
+            echo "$AUTH_WHISK_SYSTEM" > /openwhisk/ansible/files/auth.whisk.system
+        fi
 
-  # create the couchdb system databases
-  curl -X PUT http://$COUCHDB_USER:$COUCHDB_PASSWORD@localhost:$DB_PORT/_users
-  curl -X PUT http://$COUCHDB_USER:$COUCHDB_PASSWORD@localhost:$DB_PORT/_replicator
-  curl -X PUT http://$COUCHDB_USER:$COUCHDB_PASSWORD@localhost:$DB_PORT/_global_changes
+        # Fake our UID because OpenShift runs with random uids
+        export LD_PRELOAD=/usr/lib/libuid_wrapper.so
+        export UID_WRAPPER=1
+        export UID_WRAPPER_ROOT=1
 
-  pushd ansible
-    # initialize the DB
-    ansible-playbook -i environments/local initdb.yml \
-      -e db_prefix=$DB_PREFIX \
-      -e db_host=localhost \
-      -e db_username=$COUCHDB_USER \
-      -e db_password=$COUCHDB_PASSWORD \
-      -e db_port=$DB_PORT \
-      -e openwhisk_home=/openwhisk
+        # setup and initialize DB
+        pushd ansible
+          ansible-playbook -i environments/local setup.yml
+          ansible-playbook -i environments/local couchdb.yml --tags ini \
+            -e db_prefix=$DB_PREFIX \
+            -e db_host=localhost \
+            -e db_username=$COUCHDB_USER \
+            -e db_password=$COUCHDB_PASSWORD \
+            -e db_port=$DB_PORT \
+            -e openwhisk_home=/openwhisk
+        popd
 
-    # wipe the DB
-    ansible-playbook -i environments/local wipe.yml \
-      -e db_prefix=$DB_PREFIX \
-      -e db_host=localhost \
-      -e db_username=$COUCHDB_USER \
-      -e db_password=$COUCHDB_PASSWORD \
-      -e db_port=$DB_PORT \
-      -e openwhisk_home=/openwhisk
-  popd
+        # disable reduce limits on views
+        curl -X PUT http://$COUCHDB_USER:$COUCHDB_PASSWORD@localhost:$DB_PORT/_node/couchdb@$NODENAME/_config/query_server_config/reduce_limit -d '"false"'
 
-  # Unfake the UID
-  unset LD_PRELOAD UID_WRAPPER UID_WRAPPER_ROOT
-popd
+        # create the couchdb system databases
+        curl -X PUT http://$COUCHDB_USER:$COUCHDB_PASSWORD@localhost:$DB_PORT/_users
+        curl -X PUT http://$COUCHDB_USER:$COUCHDB_PASSWORD@localhost:$DB_PORT/_replicator
+        curl -X PUT http://$COUCHDB_USER:$COUCHDB_PASSWORD@localhost:$DB_PORT/_global_changes
+
+        pushd ansible
+          # initialize the DB
+          ansible-playbook -i environments/local initdb.yml \
+            -e db_prefix=$DB_PREFIX \
+            -e db_host=localhost \
+            -e db_username=$COUCHDB_USER \
+            -e db_password=$COUCHDB_PASSWORD \
+            -e db_port=$DB_PORT \
+            -e openwhisk_home=/openwhisk
+
+          # wipe the DB
+          ansible-playbook -i environments/local wipe.yml \
+            -e db_prefix=$DB_PREFIX \
+            -e db_host=localhost \
+            -e db_username=$COUCHDB_USER \
+            -e db_password=$COUCHDB_PASSWORD \
+            -e db_port=$DB_PORT \
+            -e openwhisk_home=/openwhisk
+        popd
+
+        # Unfake the UID
+        unset LD_PRELOAD UID_WRAPPER UID_WRAPPER_ROOT
+
+        # stamp that we successfully initialized the database
+        date > /opt/couchdb/data/_openwhisk_initialized.stamp
+
+    popd
+fi
 
 echo "successfully setup and configured CouchDB for OpenWhisk"
 
